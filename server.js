e4be923bcafea81da43dc145c78c97c0a3b9531f
Snow Cactus <camdenz2k3@gmail.com>
2023-02-14 21:11:05 -0600

    added get and post route

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const fs = require('fs');
 const path = require('path');
 
@@ -12,9 +12,16 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
+    fs.readFile(path.join(__dirname, "db", "db.json"), 'utf-8', function(err, data) {
+        if (err) {
+            res.status(500).json(err)
+            return
+        }
+        const json = JSON.parse(data)
+        res.json(json)
+    })
 });
-
+//view routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -23,32 +30,34 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
 
 app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
+    const {title, text} = req.body
+
+    if (!title || !text) {
+        res.status(400).json({error: 'Missing title or text.'})
+      return
+    }
+    const newNote = {
+        id: shortid.generate(),
+        ...req.body
+    }
+    fs.readFile(path.join(__dirname, "db", "db.json"), 'utf-8', function(err, data) {
+        if (err) {
+            res.status(500).json(err)
+            return
+        }
+        const notes = JSON.parse(data)
+        notes.push(newNote)
+        
+        fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(notes), function(err) {
+            if (err) {
+                res.status(500).json(err)
+                return
+            }
+            res.status(200).json(newNote)
+        })
+    })
 });
 
 function deleteNote(id, notesArray) {
@@ -73,5 +82,5 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port http://localhost:${PORT} !`);
 });
